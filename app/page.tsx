398738cf7a98fa9da337e1d05e89c6703d0fafd3
smiana123@gmail.com <smiana123@gmail.com>
2026-05-09 02:36:53 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button
+        variant={"destructive"}
+        size={"sm"}
+      >
+        Click me
+      </Button>
+      <p className="text-red-500 font-semibold">
+        AnA
+      </p>
+    </div>
+  );
+}
