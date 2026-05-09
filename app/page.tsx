80e4c18def8c4a61ffa563f844041ad0d0471eed
smiana123@gmail.com <smiana123@gmail.com>
2026-05-09 03:20:05 +0300

    add sign in page canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,8 @@
 // import Image from "next/image";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="">
-      <Button>
-        Primary
-      </Button>
-      <Button variant={"secondary"}>
-        Secondary
-      </Button>
-      <Button variant={"destructive"}>Destructive</Button>
-      <Button variant={"ghost"}>Ghost</Button>
-      <Button variant={"link"}>Link</Button>
-      <Button variant={"outline"}>Outline</Button>
-      <Button variant={"mutual"}>Mutual</Button>
-      <Button variant={"tertiary"}>Tertiary</Button>
     </div>
   );
 }
