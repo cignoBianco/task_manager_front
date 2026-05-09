6112d540fb9eddca624d9a9a3e1f0daac8f5fbd5
smiana123@gmail.com <smiana123@gmail.com>
2026-05-09 03:41:47 +0300

    add auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 interface AuthLayoutPropd {
     children: React.ReactNode;
@@ -5,10 +6,21 @@ interface AuthLayoutPropd {
 
 const AuthLayout = ({ children }: AuthLayoutPropd) => {
     return (
-        <div>
-            <Image src="/logo.svg" height={50} width={100} alt="TMLogo" />
-            {children}
-        </div>
+        <main className='bg-neutral-100 min-h-screen'>
+            <div className='mx-auto max-w-screen-2xl p-4'>
+                <nav className="flex justify-between items-center">
+                    <Image src="/logo.svg" alt={"logo"} width={152} height={56} />
+                    <div className='flex items-center gap-2'>
+                        <Button variant={"secondary"}>
+                            Sign Up
+                        </Button>
+                    </div>
+                </nav>
+                <div className='flex flex-col items-center justify-center pt-4 md:pt-14'>
+                    {children}
+                </div>
+            </div>
+        </main>
     );
 }
 
