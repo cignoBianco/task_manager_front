b2a49ffd40646f5ca28b1b45cd48a407f0c88423
smiana123@gmail.com <smiana123@gmail.com>
2026-05-09 14:05:45 +0300

    add sign up form validation

--- features/auth/components/sign-up-card.tsx
+++ features/auth/components/sign-up-card.tsx
@@ -1,5 +1,8 @@
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
+import * as z from "zod";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { DottedSeparator } from '@/components/dotted-separator';
 import { Button } from '@/components/ui/button';
@@ -11,9 +14,37 @@ import {
     CardTitle
 } from "@/components/ui/card";
 import { Input } from '@/components/ui/input';
+import {
+    Field,
+    FieldError,
+    FieldGroup
+} from "@/components/ui/field"
 import Link from 'next/link';
 
+const formSchema = z.object({
+    name: z.string()
+        .min(1, "Введите имя")
+        .max(32, "Допустимо максимум 32 символа"),
+    email: z.email("Поле обязательно для заполнения"),
+    password: z.string()
+        .min(5, "Пароль должен состоять минимум из 5 символов")
+        .max(32, "Допустимо максимум 32 символа"),
+});
+
 export const SignUpCard = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: ""
+        }
+    });
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        console.log(data)
+    }
+
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none ring-0">
             <CardHeader className="fle flex-col items-center justify-center text-center p-7 pb-3">
@@ -36,33 +67,58 @@ export const SignUpCard = () => {
                 <DottedSeparator />
             </div>
             <CardContent className='p-7 pt-3 pb-3'>
-                <form className='space-y-4'>
-                    <Input
-                        required
-                        type="text"
-                        value={""}
-                        onChange={() => { }}
-                        placeholder='Введите ваше имя'
-                        disabled={false}
-                    />
-                    <Input
-                        required
-                        type="email"
-                        value={""}
-                        onChange={() => { }}
-                        placeholder='Введите email'
-                        disabled={false}
-                    />
-                    <Input
-                        required
-                        type="passowrd"
-                        value={""}
-                        onChange={() => { }}
-                        placeholder='Введите пароль'
-                        disabled={false}
-                        min={8}
-                        max={256}
-                    />
+                <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-4'>
+                    <FieldGroup>
+                        <Controller
+                            name="name"
+                            control={form.control}
+                            render={({ field, fieldState }) =>
+                                <Field>
+                                    <Input
+                                        {...field}
+                                        type="text"
+                                        placeholder='Введите ваше имя'
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            }
+                        />
+
+                        <Controller
+                            name="email"
+                            control={form.control}
+                            render={({ field, fieldState }) =>
+                                <Field>
+                                    <Input
+                                        {...field}
+                                        type="email"
+                                        placeholder='Введите email'
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            }
+                        />
+                        <Controller
+                            name="password"
+                            control={form.control}
+                            render={({ field, fieldState }) =>
+                                <Field>
+                                    <Input
+                                        {...field}
+                                        type="password"
+                                        placeholder='Введите пароль'
+                                    />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            }
+                        />
+                    </FieldGroup>
                     <Button disabled={false} size="lg" className='w-full' >
                         Зарегистрироваться
                     </Button>
